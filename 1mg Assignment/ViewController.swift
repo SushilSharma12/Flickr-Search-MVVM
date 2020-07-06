54eeeb7aed41cbe4136e9b0e907de30f644c61ec
Sushil <rikhi02@gmail.com>
2020-07-06 14:19:41 +0530

    Initial Commit

---
+++ 1mg Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1mg Assignment
+//
+//  Created by SUSHIL SHARMA on 06/07/20.
+//  Copyright © 2020 com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
